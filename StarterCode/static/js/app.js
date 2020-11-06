aed7593910c60cc136208dc4c67747f6717ab6ce
Jamison Unruh <jamisonunruh24@gmail.com>
2020-11-06 15:37:15 -0600

    finished second chart, finished demographic table.  Both chart and table are adaptive.  Charts need final formatting.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,11 @@
 d3.json("samples.json").then((importedData) => {
     var data = importedData;
     var names = data.names
-    var samples = data.samples
+    var meta = data.metadata;
+    var samples = data.samples;
+    var tbody = d3.select("tbody");
+    //  console.log(meta[0].id)
+    // console.log(samples[0].sample_values.length)
 
     d3.select("select")
         .selectAll("option")
@@ -35,6 +39,35 @@ d3.json("samples.json").then((importedData) => {
             };
 
             Plotly.newPlot("bar", data, layout)
+            var trace2 = {
+                x: samples[0].otu_ids,
+                y: samples[0].sample_values,
+                mode: "markers",
+                size: samples[0].sample_values,
+                color: samples[0].otu_ids,
+                text: samples[0].otu_labels,
+                type: "scatter"
+            }
+
+            var data1 = [trace2]
+
+            var layout1 = {
+            title: "Samples",
+            xaxis: {title: "OTU ID"}
+            }
+
+            Plotly.newPlot("bubble", data1, layout1)
+
+            var metaLabels = ["blank", "ID: " + meta[0].id, "ethnicity: " + meta[0].ethnicity, "gender: " + meta[0].gender, "age: " + meta[0].age, "location: " + meta[0].location, "bbtype:" + meta[0].bbtype, "wfreq: " + meta[0].wfreq];
+            console.log(metaLabels)
+            var p = d3.select("body")
+                .selectAll("p")
+                .data(metaLabels)
+                .text(function(d) {
+                    return d;
+                });
+
+
         }
           
           // Call updatePlotly() when a change takes place to the DOM
@@ -56,14 +89,35 @@ d3.json("samples.json").then((importedData) => {
                 if (dataset === names[i]) {
                     x = samples[i].sample_values.slice(0,10),
                     y = samples[i].otu_ids.slice(0,10),
-                    hover = samples[i].otu_labels.slice(0,10)
+                    hover = samples[i].otu_labels.slice(0,10),
+                    x1 = samples[i].otu_ids,
+                    y1 = samples[i].sample_values,
+                    size = samples[i].sample_values,
+                    color = samples[i].otu_ids,
+                    text = samples[i].otu_labels
+
+                    var metaLabels = ["blank", "ID: " + meta[i].id, "ethnicity: " + meta[i].ethnicity, "gender: " + meta[i].gender, "age: " + meta[i].age, "location: " + meta[i].location, "bbtype:" + meta[i].bbtype, "wfreq: " + meta[i].wfreq];
+                    console.log(metaLabels)
+                    var p = d3.select("body")
+                        .selectAll("p")
+                        .data(metaLabels)
+                        .text(function(d) {
+                            return d;
+                        });
                 }
+
+
             }
           
             // Note the extra brackets around 'x' and 'y'
             Plotly.restyle("bar", "x", [x]);
             Plotly.restyle("bar", "y", [y]);
             Plotly.restyle("bar", "hovertext", [hover]);
+            Plotly.restyle("bubble", "x", [x1]);
+            Plotly.restyle("bubble", "y", [y1]);
+            Plotly.restyle("bubble", "size", [size]);
+            Plotly.restyle("bubble", "color", [color]);
+            Plotly.restyle("bubble", "text", [text]);
           }
           
         init();
